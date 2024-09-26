0e8b28212ccd7707000733176aad479871d56a9a
KaranKhulbe <karankhulbe24@gmail.com>
2024-09-26 06:33:05 +0530

    commit 5

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
+require('dotenv').config()
 const express =  require('express')
 //express app
 const app = express()
 
+//routes
+app.get('/',(req,res)=>{
+res.json({mssg: 'welcome to the app karan'})
+})
+
 //listen for request
-app.listen(3000, ()=>{
-    console.log('listening on port 3000')
+app.listen(process.env.PORT, ()=>{
+    console.log('listening on port ')
 })
