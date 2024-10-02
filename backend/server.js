79c38db3aea30bd26da6b39238a7eebac8e1ad2e
KaranKhulbe <karankhulbe24@gmail.com>
2024-10-03 00:53:59 +0530

    fixed backend bugs

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,32 @@
 require('dotenv').config()
-const express =  require('express')
-const workoutRoutes = require('./routes/workout')
+
+const express = require('express')
 const mongoose = require('mongoose')
-//express app
+const workoutRoutes = require('./routes/workouts')
+
+// express app
 const app = express()
 
-//middleware
+// middleware
 app.use(express.json())
-app.use((req,res,next)=>{
-    console.log(req.path, req.method)
-    next()
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
 })
 
-//routes
-app.use('/api/workout',workoutRoutes)
-//connect to db
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>{
-    //listen for request
-app.listen(process.env.PORT, ()=>{
-    console.log('listening on port ')
-})
-})
-.catch((error)=>{
-    console.log(error)
-})
+// routes
+app.use('/api/workouts', workoutRoutes)
 
-//listen for request
-app.listen(process.env.PORT, ()=>{
-    console.log('listening on port ')
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
