0bba1194bd1f00230ebec2b1154c207008f95278
KaranKhulbe <karankhulbe24@gmail.com>
2024-10-02 23:23:04 +0530

    added mongoose schema model

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,5 +1,6 @@
 const express= require('express')
 const router = express.Router()
+const workout = require('../models/workoutModel')
 //Get all workouts
 router.get('/',(req,res)=>{
     res.json({mssg: 'Get all workouts'})
@@ -9,8 +10,15 @@ router.get('/:id',(req,res)=>{
     res.json({mssg:'Get a single workout'})
 })
 //post a new workout
-router.post('/',(req,res)=>{
-    res.json({mssg: 'post a new workout'})
+router.post('/',async (req,res)=>{
+    const {title,load,reps} = req.body
+    try{
+       const workout = await Workout.create({title,load,reps})
+       res.status(200).json(workout)
+    }catch(error){
+     res.status(400).json({error: error.message})
+    }
+
 })
 //delete a workout
 router.delete('/:id',(req,res)=>{
