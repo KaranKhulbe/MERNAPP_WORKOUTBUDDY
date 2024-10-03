0cd350afa24b5154a4e02fda5a287ca70ff13307
KaranKhulbe <karankhulbe24@gmail.com>
2024-10-04 01:14:21 +0530

    Signup form completed

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -4,8 +4,13 @@ const Signup =()=>{
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    const handleSubmit = async(e) =>{
+    e.preventDefault()
+    console.log(email, password)
+    }
+
     return(
-        <form className="signup" onSubmit={}>
+        <form className="signup" onSubmit={handleSubmit}>
             <h3> Sign up </h3>
             <label>Email: </label>
             <input 
@@ -16,9 +21,12 @@ const Signup =()=>{
             <label>Password: </label>
             <input 
                 type="password"
-                onChange = {(e)=> setEmail(e.target.value)}
-                value={email}
+                onChange = {(e)=> setPassword(e.target.value)}
+                value={password}
             />
+            <button>sign Up </button>
         </form>
     )
 }
+
+export default Signup
