d556a58f1a6e294a034b06ede33dfb9daf2aa200
KaranKhulbe <karankhulbe24@gmail.com>
2024-10-03 22:15:02 +0530

    Added user controllers and routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,11 @@
+// login
+const loginUser = async (req,res)=>{
+    res.json({mssg:'login user'})
+}
+
+// signup
+const signupUser = async (req,res)=>{
+    res.json({mssg:'signup user'})
+}
+
+module.exports = { signupUser, loginUser}
